2f7c6fabeaba2440a769c3c46b579cb2e03cce25
PrayagDesai1 <prayagdesai1@gmail.com>
2018-07-14 17:06:05 +0530

    code

---
+++ Array/delete_item_at_given_position.py
@@ -0,0 +1,31 @@
+"""  delete(index) - delete item at index, shifting all trailing elements left """
+
+class Array(object):
+    
+    def __init__(self):
+        self.array = list()
+    
+    def insert_at_end(self, data):
+        self.array.append(data)
+    
+    def display(self):
+        if self.array is None:
+            print('Array is empty')
+        else:
+            print(self.array)
+    
+    def delete_at_perticular_position(self, position):
+        if len(self.array) >= position:
+            del self.array[position]
+        else:
+            print(' {} is invalid position cannot perform deletion '.format(position))
+            
+
+
+array = Array()
+array.insert_at_end(1)
+array.insert_at_end(2)
+array.insert_at_end(3)
+array.delete_at_perticular_position(2)
+array.delete_at_perticular_position(22)
+array.display()
