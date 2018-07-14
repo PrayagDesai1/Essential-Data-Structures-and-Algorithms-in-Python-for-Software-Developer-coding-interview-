10498d4328f8d78c38474e14d906d394ee3b8612
PrayagDesai1 <prayagdesai1@gmail.com>
2018-07-14 10:28:04 +0530

    code

---
+++ Array/return_elements_at_perticular_position.py
@@ -0,0 +1,27 @@
+""" Return array eleemnt at given position """
+
+
+class Array(object):
+    
+    def __init__(self):
+        self.array = list()
+    
+    def insert(self, data):
+        self.array.append(data)
+    
+    def display(self):
+        print(self.array)
+    
+    def return_data_at(self, position):
+        try:
+            print(self.array[position])
+        except IndexError:
+            print('Invalid position')
+
+
+array = Array()
+array.insert(1)
+array.insert(2)
+array.insert(3)
+array.display()
+array.return_data_at(3)
